ec2fc9d2d7ad8b785b9992646bd0df6756903c32
EyedJester <eyedprogrammer@gmail.com>
2017-03-09 13:51:17 -0600

    Light fixes.

--- fun.autoSpawn.js
+++ fun.autoSpawn.js
@@ -18,30 +18,33 @@ var autoSpawn = {
     }
     
     if(devotedUpgrader.length < 2 && harvesters.length == 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE, MOVE],
+        newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE, MOVE],
         undefined, {role: 'devotedUpgrader'});
         console.log('Spawning new devoted upgrader: ' + newName);
     }
     
     if(builder.length < 2 && harvesters.length == 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE],
+        newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE],
         undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
     
     if(repairMan.length < 2 && harvesters.length == 2 && builder.length == 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE],
+        newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE],
         undefined, {role: 'repairman'});
         console.log('Spawning new repairman: ' + newName);
     }
      
     if(footSoldier.length < 7 && harvesters.length == 2 && repairMan.length == 100) {
-    Game.spawns['Spawn1'].createCreep([MOVE, ATTACK, TOUGH, CARRY, TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,],
-    newName, {role: 'footSoldier'});
+      Game.spawns['Spawn1'].createCreep([MOVE, ATTACK, TOUGH, CARRY, TOUGH,TOUGH,TOUGH,
+                                         TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,
+                                         TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,],
+      newName, {role: 'footSoldier'});
     }
     
-    if(footSoldier.length < 11 && harvesters.length == 2 && harvesters.length == 1000) {
-        Game.spawns['Spawn1'].createCreep([MOVE, MOVE, ATTACK, ATTACK, ATTACK, ATTACK, CARRY, CARRY, TOUGH, TOUGH, TOUGH],
+    if(footSoldier.length < 11 && harvesters.length == 2 && harvesters.length === 1000) {
+        Game.spawns['Spawn1'].createCreep([MOVE, MOVE, ATTACK, ATTACK, ATTACK,
+                                           ATTACK, CARRY, CARRY, TOUGH, TOUGH, TOUGH],
         newName, {role: 'footSoldier'});
     }
     
@@ -49,11 +52,6 @@ var autoSpawn = {
         Game.spawns['Spawn1'].createCreep([MOVE,MOVE,MOVE,CARRY,CARRY,CARRY],
         undefined, {role: 'emCarrier'});
     }
-        
-    }
-    else {
-        
-        
         
     }
     }
