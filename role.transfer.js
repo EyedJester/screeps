0d34cd7eed7cd81ebd145a9e81fdca6517fd704a
Elijah A <eyedprogrammer@gmail.com>
2017-05-10 13:48:21 -0500

    Just changed some formatting.

--- role.transfer.js
+++ role.transfer.js
@@ -1,36 +1,35 @@
 var roleTransfer = {
-    
-    run: function(creep) {
-        
-        
-        
-        if (creep.carry.energy < creep.carryCapacity) { 
-        var supplies = creep.room.find(FIND_MY_CREEPS, {
-            filter: (creep) => {
-                return (creep.memory.role === 'harvesters') && (creep.memory.help === true);
-            }
-        });
-        creep.moveTo(supplies[0]);
-        }
-        
-        if (creep.carry.energy === creep.carryCapacity) {
-            var refill = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER);
-                }
-            }
-            )
-            
-            if (creep.transfer(refill[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(refill[0])
-            }
-            
-           }
-    
-    
-
-
-}
+	
+	run: function(creep) {
+		
+		if (creep.carry.energy < creep.carryCapacity) { 
+			
+			var supplies = creep.room.find(FIND_MY_CREEPS, {
+				filter: (creep) => {
+					return (creep.memory.role === 'harvesters') && (creep.memory.help === true);
+				}
+			});
+			
+			creep.moveTo(supplies[0]);
+		
+		}
+		
+		if (creep.carry.energy === creep.carryCapacity) {
+			
+			var refill = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_CONTAINER);
+				}
+			});
+			
+			if (creep.transfer(refill[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+				creep.moveTo(refill[0])
+			}
+			
+		}
+		
+	}
+	
 };
 
 module.exports = roleTransfer;
