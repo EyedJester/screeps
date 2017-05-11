bfdaef8aaddc7cd4460ee1568560b52b1fb43b1b
Elijah A <eyedprogrammer@gmail.com>
2017-05-11 11:22:53 -0500

    Fixed some stuff. Change it later.
    Create a function or SOMETHING that checks the path of a creep and changes it accordingly so they don't get stuck forever waiting for the harvester to die.

--- role.devotedUpgrader.js
+++ role.devotedUpgrader.js
@@ -25,9 +25,12 @@ var roleDevotedUpgrader = {
             
 			if(creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
 		    
-				if (creep.moveTo(sources[1]) == ERR_NO_PATH) {
-			
-					creep.moveTo(sources[0]);
+				if (creep.moveTo(sources[1]) === ERR_NO_PATH) {
+					
+					if creep.harvest(sources[0] === ERR_NOT_IN_RANGE) {
+						
+						creep.moveTo(sources[0]);
+					}
 	    
 				} 
 				else
