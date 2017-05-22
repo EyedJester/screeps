37ec4d6bd1c6e4cf4b08ffa2536b74ee16810008
Elijah A <eyedprogrammer@gmail.com>
2017-05-22 13:13:58 -0500

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -7,8 +7,8 @@ var roleHarvester = {
         
 	    if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
         else {
